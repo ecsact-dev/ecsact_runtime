b7a3f8115735a60698f967efbafe37465af19a28
Ezekiel Warren <zekewarren@gmail.com>
2022-08-24 09:17:30 -0700

    more package related fns

--- ecsact/codegen/plugin_entry.h
+++ ecsact/codegen/plugin_entry.h
@@ -3,10 +3,19 @@
 
 #include "ecsact/runtime/meta.h"
 
-extern "C"
-__attribute__((visibility("default")))
-void ecsact_codegen_plugin
+#ifndef ECSACT_CODEGEN_PLUGIN_API
+#	ifdef __cplusplus
+#		define ECSACT_CODEGEN_PLUGIN_API extern "C" __attribute__((visibility("default")))
+#	else
+#		define ECSACT_CODEGEN_PLUGIN_API extern __attribute__((visibility("default")))
+# endif
+#endif // ECSACT_CODEGEN_PLUGIN_API
+
+ECSACT_CODEGEN_PLUGIN_API const char* ecsact_codegen_plugin_name();
+
+ECSACT_CODEGEN_PLUGIN_API void ecsact_codegen_plugin
 	( ecsact_package_id package_id
 	);
 
+#undef ECSACT_CODEGEN_PLUGIN_API
 #endif//ECSACT_CODEGEN_PLUGIN_ENTRY_H
