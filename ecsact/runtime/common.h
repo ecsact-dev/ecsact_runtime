dd970f235207b5b07d233ed29b24d7706350a49e
Ezekiel Warren <zekewarren@gmail.com>
2022-09-07 15:03:45 -0700

    Added missing cast id for transient -> composite

--- ecsact/runtime/common.h
+++ ecsact/runtime/common.h
@@ -40,6 +40,7 @@ ECSACT_CAST_ID_FN(ecsact_action_id, ecsact_system_like_id)
 ECSACT_CAST_ID_FN(ecsact_action_id, ecsact_composite_id)
 ECSACT_CAST_ID_FN(ecsact_component_id, ecsact_composite_id)
 ECSACT_CAST_ID_FN(ecsact_transient_id, ecsact_composite_id)
+ECSACT_CAST_ID_FN(ecsact_component_like_id, ecsact_composite_id)
 
 ECSACT_CAST_ID_FN(ecsact_component_id, ecsact_decl_id)
 ECSACT_CAST_ID_FN(ecsact_transient_id, ecsact_decl_id)
