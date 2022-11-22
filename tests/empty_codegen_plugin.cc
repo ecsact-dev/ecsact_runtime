5a3c34e778ea1e4959f1aebaeacf50c630860a22
Ezekiel Warren <zekewarren@gmail.com>
2022-11-22 11:51:51 -0800

    remove empty name

--- tests/empty_codegen_plugin.cc
+++ tests/empty_codegen_plugin.cc
@@ -1,8 +1,4 @@
 #include "ecsact/codegen_plugin.h"
 
-const char* ecsact_codegen_plugin_name() {
-	return "empty";
-}
-
 void ecsact_codegen_plugin(ecsact_package_id, ecsact_codegen_write_fn_t) {
 }
