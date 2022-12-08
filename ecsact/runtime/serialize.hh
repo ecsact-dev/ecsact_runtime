ad1c69294c8ad8c8ec32e6293a434109bb2737b5
Ezekiel Warren <zekewarren@gmail.com>
2022-12-08 13:49:40 -0800

    serialize C++ api

---
+++ ecsact/runtime/serialize.hh
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <cstddef>
+#include <span>
+#include <vector>
+#include <type_traits>
+#include "ecsact/runtime/serialize.h"
+
+namespace ecsact {
+
+template<typename T>
+std::vector<std::byte> serialize(const T& component_or_action) {
+	std::vector<std::byte> bytes;
+
+	constexpr bool is_action = std::is_same_v<decltype(T::id), ecsact_action_id>;
+	constexpr bool is_component =
+		std::is_same_v<decltype(T::id), ecsact_component_id>;
+
+	static_assert(
+		!is_action && !is_component,
+		"May only serialize components or actions"
+	);
+
+	if constexpr(is_action) {
+		bytes.resize(ecsact_serialize_action_size(T::id));
+		ecsact_serialize_action(
+			T::id,
+			&component_or_action,
+			reinterpret_cast<uint8_t*>(bytes.data())
+		);
+	} else if constexpr(is_component) {
+		bytes.resize(ecsact_serialize_component_size(T::id));
+		ecsact_serialize_component(
+			T::id,
+			&component_or_action,
+			reinterpret_cast<uint8_t*>(bytes.data())
+		);
+	}
+
+	return bytes;
+}
+
+template<typename T>
+T deserialize(std::span<std::byte> serialized_component_or_action) {
+	T result;
+
+	constexpr bool is_action = std::is_same_v<decltype(T::id), ecsact_action_id>;
+	constexpr bool is_component =
+		std::is_same_v<decltype(T::id), ecsact_component_id>;
+
+	static_assert(
+		!is_action && !is_component,
+		"May only deserialize components or actions"
+	);
+	
+	int read_amount;
+	if constexpr(is_action) {
+		read_amount = ecsact_deserialize_action(
+			T::id,
+			reinterpret_cast<const uint8_t*>(serialized_component_or_action.data()),
+			&result
+		);
+	} else if constexpr(is_component) {
+		read_amount = ecsact_deserialize_component(
+			T::id,
+			reinterpret_cast<const uint8_t*>(serialized_component_or_action.data()),
+			&result
+		);
+	}
+
+	return result;
+}
+
+} // namespace ecsact
