dfba5431e28e641c83958be09e83f4e917397043
Kelwan <austin@kelway.ca>
2023-01-27 13:02:11 -0500

    Remove unused variables

--- reference/async_reference/async_reference.hh
+++ reference/async_reference/async_reference.hh
@@ -57,10 +57,6 @@ private:
 	std::atomic_bool is_connected = false;
 	std::atomic_bool is_connected_notified = false;
 
-	std::condition_variable execute_cv;
-
-	std::atomic_bool has_executed = false;
-
 	std::chrono::milliseconds tick_rate = {};
 
 	ecsact_async_request_id next_request_id();
