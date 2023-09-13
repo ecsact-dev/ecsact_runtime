45a595abef4c76b1c60119416fb587381cc7b6ec
Ezekiel Warren <zekewarren@gmail.com>
2023-09-13 16:08:31 -0700

    fix: add actual ecsact_runtime test

--- test/empty_codegen_plugin.cc
+++
@@ -1,4 +0,0 @@
-#include "ecsact/codegen_plugin.h"
-
-void ecsact_codegen_plugin(ecsact_package_id, ecsact_codegen_write_fn_t) {
-}
---
+++ test/for_each_macros_test.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gtest/gtest.h"
+#include "ecsact/runtime.h"
+
+TEST(ForEach, ForEachLambda) {
+#define AS_PTR_VARIABLE(method_name, unused) decltype(&method_name) method_name##_ptr = nullptr;
+  
+  FOR_EACH_ECSACT_API_FN(AS_PTR_VARIABLE, unused);
+
+#undef AS_PTR_VARIABLE
+}
