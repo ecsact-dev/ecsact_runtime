5df41327415be29ea1ca1493872e63b1d6692c79
Ezekiel Warren <zaucy@users.noreply.github.com>
2024-08-13 17:46:30 -0700

    fix: add missing 'async to FOR_EACH_ECSACT_API_FN macro (#259)

--- ecsact/runtime.h
+++ ecsact/runtime.h
@@ -10,12 +10,14 @@
 #include "ecsact/runtime/static.h"
 #include "ecsact/runtime/meta.h"
 #include "ecsact/runtime/serialize.h"
+#include "ecsact/runtime/async.h"
 
-#define FOR_EACH_ECSACT_API_FN(fn, ...)            \
-	FOR_EACH_ECSACT_CORE_API_FN(fn, __VA_ARGS__);    \
-	FOR_EACH_ECSACT_DYNAMIC_API_FN(fn, __VA_ARGS__); \
-	FOR_EACH_ECSACT_META_API_FN(fn, __VA_ARGS__);    \
-	FOR_EACH_ECSACT_STATIC_API_FN(fn, __VA_ARGS__);  \
-	FOR_EACH_ECSACT_SERIALIZE_API_FN(fn, __VA_ARGS__)
+#define FOR_EACH_ECSACT_API_FN(fn, ...)              \
+	FOR_EACH_ECSACT_CORE_API_FN(fn, __VA_ARGS__);      \
+	FOR_EACH_ECSACT_DYNAMIC_API_FN(fn, __VA_ARGS__);   \
+	FOR_EACH_ECSACT_META_API_FN(fn, __VA_ARGS__);      \
+	FOR_EACH_ECSACT_STATIC_API_FN(fn, __VA_ARGS__);    \
+	FOR_EACH_ECSACT_SERIALIZE_API_FN(fn, __VA_ARGS__); \
+	FOR_EACH_ECSACT_ASYNC_API_FN(fn, __VA_ARGS__)
 
 #endif // ECSACT_RUNTIME_H
